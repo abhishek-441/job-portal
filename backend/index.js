75de425a69fb4091a02d10f0bcae9c80dce9370e
abhishek kumar <abhishekkumar@abhisheks-MacBook-Air-2.local>
2026-09-21 18:24:55 +0530

    Fix production CORS

--- backend/index.js
+++ backend/index.js
@@ -16,10 +16,19 @@ import applicationRoute from "./routes/application.route.js"
  app.use(express.json());
  app.use(express.urlencoded({ extended: true}));
  app.use(cookieParser());
- const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
- }
+//  const corsOptions = {
+//     origin:'http://localhost:5173',
+//     credentials:true
+//  }
+
+const corsOptions = {
+    origin: [
+        "http://localhost:5173",
+        "https://job-portal-three-wine.vercel.app"
+    ],
+    credentials: true
+};
+
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
